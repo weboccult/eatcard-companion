07d970bf9f629813a8681c9f9c96a7bc23df4619
Darshit Hedpara <darshithedpara@gmail.com>
2021-12-27 15:18:58 +0530

    Updates - logger type enum added

---
+++ src/Enums/LoggerTypes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Weboccult\EatcardCompanion\Enums;
+
+/**
+ * Class LoggerTypes.
+ */
+class LoggerTypes extends BaseEnum
+{
+    public const FILE = 'FILE';
+    public const CLOUDWATCH = 'CLOUDWATCH';
+}
