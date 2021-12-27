88cb583d3b7962af52a81c60abb5775a457f1398
Darshit Hedpara <darshithedpara@gmail.com>
2021-12-27 15:07:55 +0530

    Updates - new model added - helper functions added and updated - package helper added

--- src/Helpers/general.php
+++ src/Helpers/general.php
@@ -2,11 +2,12 @@
 
 namespace Weboccult\EatcardCompanion\Helpers;
 
+use Exception;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Weboccult\EatcardCompanion\Models\Order;
 use Weboccult\EatcardCompanion\Models\OrderHistory;
+use Weboccult\EatcardCompanion\Models\Supplement;
 
 if (! function_exists('eatcardSayHello')) {
     /**
@@ -24,17 +25,17 @@ if (! function_exists('eatcardSayHello')) {
 
 if (! function_exists('splitDigits')) {
     /**
-     * @description Split and return splited or diff od splited value
+     * @description Split and return split or diff od split value
      *
      * @param $val
-     * @param $split_digit
-     * @param $return_diff
-     * @param $return_digit
-     * @param $payment_digit
+     * @param int $split_digit
+     * @param bool $return_diff
+     * @param int $return_digit
+     * @param int $payment_digit
      *
-     * @return $rounded_digit
+     * @return float|int $rounded_digit
      */
-    function splitDigits($val, $split_digit = 4, $return_diff = false, $return_digit = 4, $payment_digit = 2)
+    function splitDigits($val, int $split_digit = 4, bool $return_diff = false, int $return_digit = 4, int $payment_digit = 2)
     {
         $rounded_digit = 0;
         try {
@@ -47,8 +48,12 @@ if (! function_exists('splitDigits')) {
             }
 
             return $rounded_digit;
-        } catch (\Exception $exception) {
-            Log::info('splitDigits function exception  Line : '.$exception->getLine().' | error : '.$exception->getMessage());
+        } catch (Exception $exception) {
+            companionLogger(
+                'splitDigits function exception',
+                'Line - '.$exception->getLine(),
+                'Error - '.$exception->getMessage()
+            );
 
             return $rounded_digit;
         }
@@ -88,8 +93,9 @@ if (! function_exists('getDutchDate')) {
             'November' => 'november',
             'December' => 'december',
         ];
-        $day = $day2let ? appDutchDay2Letter(Carbon::parse($date)->format('l')) : $dutchDayNames[Carbon::parse($date)
-            ->format('l')];
+        $day = $day2let ?
+            appDutchDay2Letter(Carbon::parse($date)->format('l')) :
+            $dutchDayNames[Carbon::parse($date)->format('l')];
 
         return $day.' '.Carbon::parse($date)->format('d').' '.$monthNames[Carbon::parse($date)
                 ->format('F')].' '.Carbon::parse($date)->format('Y');
@@ -140,6 +146,24 @@ if (! function_exists('appDutchDate')) {
     }
 }
 
+if (! function_exists('getS3File')) {
+
+    /**
+     * @param string $path
+     *
+     * @return string
+     * @Description get file from given path (s3)
+     */
+    function getS3File(string $path = ''): string
+    {
+        if ($path) {
+            return $path;
+        } else {
+            return env('AWS_URL').'assets/no_image.png';
+        }
+    }
+}
+
 if (! function_exists('generatePOSOrderId')) {
     /**
      * @param $store_id
@@ -148,39 +172,170 @@ if (! function_exists('generatePOSOrderId')) {
      */
     function generatePOSOrderId($store_id)
     {
-        $order_history = OrderHistory::where('store_id', $store_id)->where('order_type', 'pos')->where(function ($q) {
-            $q->where(DB::raw('LENGTH(order_id)'), '=', '8')->where('order_id', 'LIKE', Carbon::now()
-                        ->format('y').'0%');
-        })
-
-                ->whereNotBetween('created_at', [
+        $order_history = OrderHistory::query()
+            ->where('store_id', $store_id)
+            ->where('order_type', 'pos')
+            ->where(function ($q) {
+                $q->where(DB::raw('LENGTH(order_id)'), '=', '8')
+                  ->where('order_id', 'LIKE', Carbon::now()->format('y').'0%');
+            })
+            ->whereNotBetween('created_at', [
                 '2021-03-11 00:00:00',
                 '2021-03-12 23:59:59',
-            ])->orderBy('created_at', 'desc')->first();
+            ])
+            ->orderBy('created_at', 'desc')
+            ->first();
         if ($order_history) {
             $order_history->makeHidden(['full_name']);
         }
-        $order = Order::where('store_id', $store_id)->where('order_type', 'pos')->where(function ($q) {
-            $q->where(DB::raw('LENGTH(order_id)'), '=', '8')
-                    ->where('order_id', 'LIKE', Carbon::now()->format('y').'0%');
-        })
-                //            ->whereDate('created_at',date('Y-m-d'))
-                ->orderBy('created_at', 'desc')->first();
+        $order = Order::query()
+                        ->where('store_id', $store_id)
+                        ->where('order_type', 'pos')
+                        ->where(function ($q) {
+                            $q->where(DB::raw('LENGTH(order_id)'), '=', '8')
+                              ->where('order_id', 'LIKE', Carbon::now()->format('y').'0%');
+                        })
+                        ->orderBy('created_at', 'desc')
+                        ->first();
 
         if ($order) {
             $order->makeHidden(['full_name']);
         }
         if ($order && $order_history) {
-            //            Log::info('order & order history '.$order_history->id);
             if ($order_history->id > $order->id) {
-                //                Log::info(' order history > order '. $order->id.' '.$order_history->id);
                 $order = $order_history;
             }
         } elseif ($order_history) {
-            //            Log::info('only order history '.$order_history->id);
             $order = $order_history;
         }
-        //        Log::info('new order id + 1 '.$order->order_id);
+
         return $order ? $order->order_id + 1 : date('y').'000001';
+    }
+}
+
+if (! function_exists('cartTotalValueCalc')) {
+    /**
+     * @param $items
+     * @param $products
+     * @param $res
+     * @param $ayce_amount
+     *
+     * @return float|int|void
+     */
+    function cartTotalValueCalc($items, $products, $res, $ayce_amount)
+    {
+        try {
+            $product_total = 0;
+            foreach ($items as $key => $item) {
+                /*This variable used for checked product and it/s supplement price count or not in total price*/
+                $is_product_chargable = true;
+                companionLogger('item id : ', $item['id']);
+                $product = $products->where('id', $item['id'])->first();
+                $isVoided = (isset($item['void_id']) && $item['void_id'] != '');
+                $isOnTheHouse = (isset($item['on_the_house']) && $item['on_the_house'] == '1');
+                // if product is void and onthenhouse then no need to calculate.
+                if ($isVoided || $isOnTheHouse) {
+                    continue;
+                }
+                $aycePrice = 0;
+                if (isset($res) && $res) {
+                    //check ayce price
+                    if ((isset($product->ayce_class) && ! empty($product->ayce_class)) && $product->ayce_class->count() > 0 && $res->dineInPrice && isset($res->dineInPrice->dinein_category_id) && $res->dineInPrice->dinein_category_id != '') {
+                        $ayeClasses = $product->ayce_class->pluck('dinein_category_id')->toArray();
+                        if (! empty($ayeClasses) && in_array($res->dineInPrice->dinein_category_id, $ayeClasses) /*&& $product->all_you_can_eat_price > 0*/) {
+                            $ayce_individual_price = $product->ayce_class->where('dinein_category_id', $res->dineInPrice->dinein_category_id)
+                                ->pluck('price');
+                            if (isset($ayce_individual_price[0]) && $ayce_individual_price[0] > 0 && ! empty($ayce_individual_price[0]) && $product->all_you_can_eat_price >= 0) {
+                                $aycePrice = $ayce_individual_price[0];
+                            } else {
+                                if (! empty($product->all_you_can_eat_price)) {
+                                    $aycePrice = $product->all_you_can_eat_price;
+                                }
+                            }
+                        }
+                    } else {
+                        /*If res type is cart then get product price from pieces*/
+                        if (isset($product->total_pieces) && $product->total_pieces != '' && isset($product->pieces_price) && $product->pieces_price != '' && $res->reservation_type != 'all_you_eat') {
+                            $aycePrice = (float) $product->pieces_price;
+                        }
+                    }
+                }
+                companionLogger('Product ayce price : ', $aycePrice);
+                if ($aycePrice) {
+                    //if there is ayce price
+                    $product->price = $aycePrice;
+                } else {
+                    /*check for if the category is free or not if the order is from the all you cat eat functionality*/
+                    if (! $item['base_price']) {
+                        $product->price = 0;
+                        $is_product_chargable = false;
+                    } else {
+                        $product->price = (! is_null($product->discount_price) && $product->discount_show) ? $product->discount_price : $product->price;
+                    }
+                }
+                companionLogger('Product price', $product->name, $product->price);
+                if ($product->price > 0 || $is_product_chargable) {
+                    $supplement_total = 0;
+                    $supplements = [];
+                    if (isset($item['supplements'])) {
+                        $supplement_ids = collect($item['supplements'])->pluck('id')->toArray();
+                        $supplements = Supplement::withTrashed()->whereIn('id', $supplement_ids)->get();
+                        $newSup = [];
+                        foreach (collect($item['supplements']) as $i) {
+                            $isExist = collect($newSup)->search(function ($item) use ($i) {
+                                return $item['id'] == $i['id'] && $item['val'] == $i['val'];
+                            });
+                            if ($isExist && $i['val'] == $newSup[$isExist]['val']) {
+                                $newSup[$isExist]['qty'] += 1;
+                                $newSup[$isExist]['total_val'] = $newSup[$isExist]['val'] * $newSup[$isExist]['qty'];
+                            } else {
+                                $newSup[] = [
+                                    'id'        => $i['id'],
+                                    'name'      => $i['name'],
+                                    'val'       => $i['val'] ?? 0,
+                                    'total_val' => $i['val'] ?? 0,
+                                    'qty'       => isset($i['qty']) && $i['qty'] ? $i['qty'] : 1,
+                                ];
+                            }
+                        }
+                        $item['supplements'] = $newSup;
+                    }
+                    foreach ($item['supplements'] as $supp) {
+                        $currentSup = collect($supplements)->where('id', $supp['id'])->first();
+                        if ($supp['val'] != 0) {
+                            $supplement_total += $currentSup->price * $supp['qty'];
+                        }
+                    }
+                    $size_total = 0;
+                    if (isset($item['size']) && $item['size']) {
+                        if ($item['size']['name'] == 'large') {
+                            $size_total = $product->large_price;
+                        } elseif ($item['size']['name'] == 'regular') {
+                            $size_total = $product->regular_price;
+                        }
+                    }
+                    $weight_total = $product->price;
+                    if (isset($item['weight']) && $item['weight']) {
+                        $weight_total = ((int) $item['weight'] * $product->price) / $product->weight;
+                    }
+                    if ($is_product_chargable) {
+                        $product_total += ($supplement_total + $size_total + $weight_total) * $item['quantity'];
+                    } else {
+                        $product_total += 0;
+                    }
+                }
+            }
+            $product_total += ! empty($ayce_amount) ? (float) $ayce_amount : 0;
+
+            return $product_total;
+        } catch (Exception $e) {
+            companionLogger(
+                'cartTotalValueCalc',
+                'error - '.$e->getMessage(),
+                'IP address - '.request()->ip(),
+                'Browser - '.request()->header('User-Agent'),
+                'Line - '.$e->getLine()
+            );
+        }
     }
 }
--- src/Helpers/pacakgeHelper.php
+++ src/Helpers/pacakgeHelper.php
@@ -1,5 +1,39 @@
 <?php
 
+use Weboccult\EatcardCompanion\Enums\LoggerTypes;
+use Illuminate\Support\Facades\Log;
+
+if (! function_exists('companionLogger')) {
+    /**
+     * Access companionLogger through helper.
+     *
+     * @param mixed ...$values
+     *
+     * @return void
+     */
+    function companionLogger(...$values): void
+    {
+        $driver = 'FILE';
+        $settings = config('eatcardCompanion');
+        if (! empty($settings) && ! empty($settings['logger'] && $settings['logger']['enabled'] == true)) {
+            try {
+                if (LoggerTypes::isValidName($settings['logger']['driver'])) {
+                    $driver = $settings['logger']['driver'];
+                }
+            } catch (Exception $e) {
+            }
+        }
+        $logContent = collect($values)->pipeInto(\Illuminate\Support\Stringable::class)->jsonSerialize();
+        switch ($driver) {
+            case LoggerTypes::FILE:
+                Log::info($logContent);
+                break;
+            case LoggerTypes::CLOUDWATCH:
+                break;
+        }
+    }
+}
+
 if (! function_exists('eatcardPrint')) {
     /**
      * Access eatcardPrint through helper.
